17d05ce221fdc8b61ca911a0f00fac094fedbad1
dsllt <dayanesallet@hotmail.com>
2025-03-01 16:15:31 -0300

    feat: creates migration for tables schedules and menus

---
+++ src/main/resources/db/migration/prod/V2__create-schedule-and-menu.sql
@@ -0,0 +1,23 @@
+CREATE TYPE week_days AS ENUM ('monday', 'tuesday', 'wednesday', 'thursday', ' friday', 'saturday', 'sunday');
+
+CREATE TABLE schedules (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  week_day WEEK_DAYS,
+  open_at TIMETZ,
+  close_at TIMETZ,
+  place_id UUID,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TYPE food_type AS ENUM ('food', 'drink');
+
+CREATE TABLE menus (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(200),
+  type FOOD_TYPE,
+  price DECIMAL(8,2),
+  place_id UUID,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
